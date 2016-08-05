50218188259e238ba6003582af2699f07df2206e
jellee68 <jjlee460@yahoo.com>
2016-08-05 19:11:56 +0100

    Updated

--- Headers/module.h
+++ Headers/module.h
@@ -3,29 +3,44 @@
 
 using namespace std ;
 
+class Dog
+{
+	int age, weight ;
+	string colour ;
+
+	public:
+		void bark() { cout << "WOOF!" << endl ; }
+
+		void setAge( int yrs) 		{ age = yrs ; } 
+		void setWeight( int lbs) 	{ weight = lbs ; } 
+		void setColour( string hue) 	{ colour = hue ; }
+		
+		int getAge() 		{ return age; 	}
+		int getWeight() 	{ return weight; 	}
+		string getColour() 	{ return colour; }
+} ;
+
+
 void print(string word )
     {
         cout << word << endl ;
     }
     
-string myGoodFunction(const char* charArray)
+string shiftLeft(const char* charArray)
 	{
 		int i = 30 ;
 		int length ;
 		string measurable = string(charArray) ;
 		for( i=0 ; i<30 ; i++)
 			{
-				cout << measurable[i] << endl ;
+				measurable[i] = measurable[ i+1] ;
 				if (measurable[i] == '\0')
-				{ 
-					cout << "The end" << endl ;
-					length = i ;
-					break ;	
-				}	
+					{	 
+						length = i ;
+						break ;	
+					}	
 			}
     cout << length << endl;
     
-    cout << x << endl ;
-    //cout << charArray[2] << endl ;
-    return string(charArray);
+    return string (measurable);
 	}
--- Headers/test.cpp
+++ Headers/test.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <string>
 #include "module.h"
 using namespace std;
 
 
 int main()
 {
-	char word[]={"Some longer String\n"} ;
-	cout << myGoodFunction(word);
-	print("FishFace");
+	string display ;							// Test shift left 
+	char word[]={"Some longer String\n"} ;		// and print functions
+	display = shiftLeft(word);					// in module.h
+	print(display) ;
+	
+	Dog fido ; 
+	
+	fido.setAge(3) ;
+	fido.setWeight(15) ;
+	fido.setColour("Brown") ;
+	
+	cout << "Fido is a " << fido.getColour() << " dog" << endl ;
+	cout << "Fido is " << fido.getAge() << " years old" << endl ;
+	cout << "Fido weighs " << fido.getWeight() << " pounds" << endl ;
+
 	
 	
 	return 0 ;
